eba7708b428aa1439627f5aa999c5dd0ef2efc34
mtsuno24 <160788307+mtsuno24@users.noreply.github.com>
2024-07-29 16:58:06 +0900

    ファイルをアップロード

---
+++ kadai_020/event.js
@@ -0,0 +1,8 @@
+const btn = document.getElementById('btn');
+
+const newText = document.getElementById('text');
+
+btn.addEventListener('click', () => {
+  newText.textContent = 'ボタンをクリックしました';
+  console.log(newText);
+});
